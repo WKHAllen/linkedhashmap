667759af3d6b15f96b86fab731c47f6188ecab15
WKHAllen <wkhallen@gmail.com>
2025-02-10 18:19:42 -0500

    Separate tests into individual functions

--- test/test.c
+++ test/test.c
@@ -156,81 +156,141 @@ void sum_ints(void* key, size_t key_size, void* value, void* total)
     *(int*)total += *(int*)value;
 }
 
-int main(void)
-{
-    // Begin
-    printf("Beginning tests\n");
+#define INIT_SQUARES() \
+    LinkedHashMap* map = linkedhashmap_new(); \
+    int indices[17]; \
+    int squares[17]; \
+    for (int i = 0; i < 17; i++) \
+    { \
+        indices[i] = i; \
+        squares[i] = i * i; \
+        if (i < 16) \
+            linkedhashmap_set(map, &(indices[i]), sizeof(indices[i]), &(squares[i])); \
+    } \
 
-    // test initialization
-    // { }
+// test initialization and deallocation
+// { }
+void test_init(void)
+{
     LinkedHashMap* map = linkedhashmap_new();
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)0);
     TEST_ASSERT(linkedhashmap_is_empty(map));
     TEST_ASSERT_EQ(map->length, (size_t)0);
     TEST_ASSERT_EQ(map->capacity, (size_t)LINKEDHASHMAP_MIN_SIZE);
 
-    // test hash function
+    linkedhashmap_free(map);
+}
+
+// test hash function
+void test_hash(void)
+{
+    LinkedHashMap* map = linkedhashmap_new();
+
     int int_key = 123;
     size_t int_hash = linkedhashmap_hash(map, &int_key, sizeof(int));
     TEST_ASSERT_EQ(int_hash, (size_t)(123 % map->capacity));
+
     char* str_key = "abcde";
     size_t str_hash = linkedhashmap_hash(map, str_key, STR_SIZE(str_key));
     TEST_ASSERT_EQ(str_hash, (size_t)(495 % map->capacity));
 
-    // test set
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
-    int indices[17];
-    int squares[17];
-    for (int i = 0; i < 17; i++)
-    {
-        indices[i] = i;
-        squares[i] = i * i;
-        if (i < 16)
-            linkedhashmap_set(map, &(indices[i]), sizeof(indices[i]), &(squares[i]));
-    }
+    linkedhashmap_free(map);
+}
+
+// test set
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_set(void)
+{
+    INIT_SQUARES();
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)16);
     TEST_ASSERT(!linkedhashmap_is_empty(map));
     TEST_ASSERT_EQ(map->length, (size_t)16);
     TEST_ASSERT_EQ(map->capacity, (size_t)16);
 
-    // test get
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+    linkedhashmap_free(map);
+}
+
+// test get
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_get(void)
+{
+    INIT_SQUARES();
+
     for (int i = 0; i < 16; i++)
         TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map, &(indices[i]), sizeof(indices[i])), squares[i]);
 
-    // test resize_up
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 256 }
+    linkedhashmap_free(map);
+}
+
+// test resize_up
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 256 }
+void test_resize_up(void)
+{
+    INIT_SQUARES();
+
     linkedhashmap_set(map, &(indices[16]), sizeof(indices[16]), &(squares[16]));
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)17);
     TEST_ASSERT_EQ(map->length, (size_t)17);
     TEST_ASSERT_EQ(map->capacity, (size_t)32);
+
     for (int i = 0; i < 17; i++)
         TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map, &(indices[i]), sizeof(indices[i])), squares[i]);
 
-    // test pop and resize_down
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+    linkedhashmap_free(map);
+}
+
+// test pop and resize_down
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_pop_resize_down(void)
+{
+    INIT_SQUARES();
+
+    linkedhashmap_set(map, &(indices[16]), sizeof(indices[16]), &(squares[16]));
+
     TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_pop(map, &(indices[16]), sizeof(indices[16])), squares[16]);
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)16);
     TEST_ASSERT_EQ(map->length, (size_t)16);
     TEST_ASSERT_EQ(map->capacity, (size_t)16);
+
     for (int i = 0; i < 16; i++)
         TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map, &(indices[i]), sizeof(indices[i])), squares[i]);
 
-    // test copy and equal
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+    linkedhashmap_free(map);
+}
+
+// test copy and equal
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_copy_equal(void)
+{
+    INIT_SQUARES();
+
     LinkedHashMap* map2 = linkedhashmap_copy(map);
+
     for (int i = 0; i < 16; i++)
         TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map2, &(indices[i]), sizeof(indices[i])), squares[i]);
+
     TEST_ASSERT(linkedhashmap_equal(map, map2));
 
-    // test extend
-    // { 0: 0, 1: 1, 2: 4, 3: 21, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 37 }
+    linkedhashmap_free(map);
+}
+
+// test extend
+// { 0: 0, 1: 1, 2: 4, 3: 21, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 37 }
+void test_extend(void)
+{
+    INIT_SQUARES();
+
+    LinkedHashMap* map2 = linkedhashmap_copy(map);
     int map3value1 = 21;
     int map3value2 = 37;
     LinkedHashMap* map3 = linkedhashmap_new();
     linkedhashmap_set(map3, &(indices[3]), sizeof(map3value1), &(map3value1));
     linkedhashmap_set(map3, &(indices[16]), sizeof(map3value2), &(map3value2));
     linkedhashmap_extend(map2, map3);
+
     TEST_ASSERT(!linkedhashmap_equal(map, map2));
     TEST_ASSERT(!linkedhashmap_equal(map, map3));
     TEST_ASSERT(!linkedhashmap_equal(map2, map3));
@@ -238,19 +298,29 @@ int main(void)
     TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map2, &(indices[16]), sizeof(indices[16])), map3value2);
     TEST_ASSERT_EQ(linkedhashmap_length(map2), (size_t)17);
 
-    // test keys, values, and entries
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+    linkedhashmap_free(map);
+}
+
+// test keys, values, and entries
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_keys_values_entries(void)
+{
+    INIT_SQUARES();
+
     int key_total = 0;
     int value_total = 0;
     int entries_total = 0;
+
     for (int i = 0; i < 16; i++)
     {
         indices[i] = i;
         squares[i] = i * i;
     }
+
     void** map_keys = linkedhashmap_keys(map);
     void** map_values = linkedhashmap_values(map);
     void** map_entries = linkedhashmap_entries(map);
+
     for (size_t i = 0; i < linkedhashmap_length(map); i++)
     {
         key_total += *(int*)(map_keys[i]);
@@ -258,55 +328,129 @@ int main(void)
         entries_total += *(int*)(map_entries[2 * i]);
         entries_total += *(int*)(map_entries[2 * i + 1]);
     }
+
     TEST_ASSERT_INT_EQ(key_total, 120);
     TEST_ASSERT_INT_EQ(value_total, 1240);
     TEST_ASSERT_INT_EQ(entries_total, key_total + value_total);
+
     free(map_keys);
     free(map_values);
     free(map_entries);
 
-    // test delete
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 256 }
+    linkedhashmap_free(map);
+}
+
+// test delete
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225, 16: 256 }
+void test_delete(void)
+{
+    INIT_SQUARES();
+
     linkedhashmap_set(map, &(indices[16]), sizeof(indices[16]), &(squares[16]));
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)17);
     TEST_ASSERT_EQ(map->length, (size_t)17);
     TEST_ASSERT_EQ(map->capacity, (size_t)32);
+
     linkedhashmap_delete(map, &(indices[16]), sizeof(indices[16]));
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)16);
     TEST_ASSERT_EQ(map->length, (size_t)16);
     TEST_ASSERT_EQ(map->capacity, (size_t)16);
+
     for (int i = 0; i < 16; i++)
         TEST_ASSERT_INT_EQ(*(int*)linkedhashmap_get(map, &(indices[i]), sizeof(indices[i])), squares[i]);
 
-    // test contains
-    // { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+    linkedhashmap_free(map);
+}
+
+// test contains
+// { 0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225 }
+void test_contains(void)
+{
+    INIT_SQUARES();
+
     for (int i = 0; i < 16; i++)
         TEST_ASSERT(linkedhashmap_contains(map, &(indices[i]), sizeof(indices[i])));
+
     TEST_ASSERT(!linkedhashmap_contains(map, &(indices[16]), sizeof(indices[16])));
 
-    // test clear
-    // { }
+    linkedhashmap_free(map);
+}
+
+// test clear
+// { }
+void test_clear(void)
+{
+    INIT_SQUARES();
+
     linkedhashmap_clear(map);
+
     TEST_ASSERT_EQ(linkedhashmap_length(map), (size_t)0);
     TEST_ASSERT(linkedhashmap_is_empty(map));
     TEST_ASSERT_EQ(map->length, (size_t)0);
     TEST_ASSERT_EQ(map->capacity, (size_t)LINKEDHASHMAP_MIN_SIZE);
 
-    // test foreach
+    linkedhashmap_free(map);
+}
+
+// test foreach
+void test_foreach(void)
+{
+    INIT_SQUARES();
+
+    linkedhashmap_clear(map);
+
     int intvals[] = { 1, 3, 7, 9 };
     int arraylen = sizeof(intvals) / sizeof(intvals[0]);
     int expected = 0;
+
     for (int i = 0; i < arraylen; i++)
     {
         linkedhashmap_set(map, &(indices[i]), sizeof(indices[i]), &(intvals[i]));
         expected += intvals[i];
     }
+
     int result = 0;
     linkedhashmap_foreach(map, sum_ints, &result);
+
     TEST_ASSERT_INT_EQ(result, expected);
 
-    // test memory deallocation
     linkedhashmap_free(map);
+}
+
+int main(void)
+{
+    // Begin
+    printf("Beginning tests\n");
+
+    // Run tests
+    printf("\nTesting initialization...\n");
+    test_init();
+    printf("\nTesting hash function...\n");
+    test_hash();
+    printf("\nTesting set...\n");
+    test_set();
+    printf("\nTesting get...\n");
+    test_get();
+    printf("\nTesting resize up...\n");
+    test_resize_up();
+    printf("\nTesting pop and resize down...\n");
+    test_pop_resize_down();
+    printf("\nTesting copy and equal...\n");
+    test_copy_equal();
+    printf("\nTesting extend...\n");
+    test_extend();
+    printf("\nTesting keys, values, and entries...\n");
+    test_keys_values_entries();
+    printf("\nTesting delete...\n");
+    test_delete();
+    printf("\nTesting contains...\n");
+    test_contains();
+    printf("\nTesting clear...\n");
+    test_clear();
+    printf("\nTesting foreach...\n");
+    test_foreach();
 
     // Done
     printf("\nCompleted tests\n");
